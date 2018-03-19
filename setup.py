f9c3a7aeb2bcdee5915ed9a5116914a718607515
Jan Aaron Angelo T. Lee <janaaronlee@gmail.com>
2018-03-19 23:03:51 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+
+install_requires = [
+    'chalice==1.1.1',
+    'pyyaml==3.12'
+]
+
+setup(
+    name='wallice',
+    version='1.1.1',
+    description="Opinionated AWS Lambda web framework",
+    long_description=README,
+    author="Jan Aaron Angelo T. Lee",
+    author_email='janaaronlee@gmail.com',
+    url='https://github.com/janaaronlee/wallice',
+    packages=find_packages(exclude=['tests']),
+    install_requires=install_requires,
+    license="MIT License",
+    package_data={'chalice': ['*.json']},
+    include_package_data=True,
+    zip_safe=False,
+    keywords='chalice',
+    entry_points={
+        'console_scripts': [
+            'wallice = wallice.cli:main',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
