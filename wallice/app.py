1ebb927529a995e2d6c9c8aec586f44dc889511a
Jan Aaron Angelo T. Lee <janaaronlee@gmail.com>
2018-03-20 20:59:44 +0800

    removed yaml config

--- wallice/app.py
+++ wallice/app.py
@@ -1,6 +1,5 @@
 from chalice import Chalice
 
-from wallice.config import Config
 from wallice.routers import ImplicitRouter
 
 
@@ -11,11 +10,5 @@ class Wallice(Chalice):
 
     def __init__(self, router=ImplicitRouter, *args, **kwargs):
 
-        self.config.update(kwargs)
-        super().__init__(*args, **self.config)
+        super().__init__(*args, **kwargs)
         router(self)
-
-    @property
-    def config(self):
-        config = Config()
-        return config['app']
