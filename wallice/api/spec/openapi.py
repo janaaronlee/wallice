7c8bab1909d28e7addc988040d996b3f18fa5e3b
Jan Aaron Angelo Lee <janaaronlee@gmail.com>
2018-04-06 21:10:38 +0800

    Add facility for custom CORS

--- wallice/api/spec/openapi.py
+++ wallice/api/spec/openapi.py
@@ -1,42 +1,110 @@
 import json
 import os
-import yaml
+
+from chalice import CORSConfig
+
+from wallice.api.spec import OpenApi
+from wallice.utils.introspector import walk
 
 
-from wallice.api.spec.schemas import OpenApiSchema
+class Router(object):
 
+    _api_file = 'api.yml'
+    _paths_dir = 'paths'
 
-class OpenApi(object):
+    def __init__(self, app, api_file=None):
+        self.app = app
 
-    def __init__(self, yaml_path):
+        if api_file is not None:
+            self._api_file = api_file
 
-        if not os.path.exists(yaml_path):
-            msg = 'OpenAPI spec file not found in: `{}`'
-            raise FileNotFoundError(msg.format(yaml_path))
+        self.route_handlers = RouteHandlers(self.paths_dir)
+        self.api = OpenApi(self.yaml_path)
+        self.add_routes()
 
-        with open(yaml_path) as f:
-            yaml_str = f.read()
+    @property
+    def yaml_path(self):
+        return os.path.join(self.app.lib_dir, self._api_file)
 
-        self.api, errors = OpenApiSchema().load(yaml.load(yaml_str))
+    @property
+    def paths_dir(self):
+        return os.path.join(self.app.lib_dir, self._paths_dir)
 
-        for error in errors:
-            raise Exception(
-                'An error has occurred while validating the OpenAPI spec '
-                'file in `{}`! Please check the `{}` field and resolve this '
-                'error.'.format(yaml_path, error)
+    def init_route(self, path, method, config):
+        function = self.route_handlers[path][method]
+
+        def wrapped(*args, **kwargs):
+            return function(self.app.current_request)
+
+        return wrapped
+
+    @property
+    def route_kwargs(self):
+        if not os.environ.get('cors', 'false').lower() == 'true':
+            return {}
+
+        return {
+            'cors': CORSConfig(
+                allow_origin=os.environ.get('cors_origin', '*'),
+                allow_headers=os.environ.get(
+                    'cors_headers',
+                    'Authorization,Content-Type,'
+                    'X-Amz-Date,X-Amz-Security-Token,X-Api-Key'
+                ).split(',')
             )
+        }
 
-    @property
-    def info(self):
-        return self.api['info']
+    def add_routes(self):
+        registered_routes = {}
+        for path, methods in self.api.paths.items():
+            for method, config in methods.items():
+                name = '{}:{}'.format(path.replace('/', '_'), method)
+                method = method.upper()
+                self.app._add_route(
+                    path, self.init_route(path, method, config),
+                    methods=[method], name=name, **self.route_kwargs
+                )
+                registered_routes.setdefault(path, [])
+                registered_routes[path].append(method)
 
-    @property
-    def servers(self):
-        return self.api.get('servers', [])
+        print('Registered routes:')
+        print(json.dumps(registered_routes, indent=2))
 
-    @property
-    def paths(self):
-        return self.api['paths']
 
-    def __str__(self):
-        return yaml.dump(self.api, default_flow_style=False)
+class RouteHandlers(dict):
+
+    valid_http_methods = (
+        'GET', 'POST', 'PUT', 'DELETE',
+        'HEAD', 'OPTIONS', 'CONNECT'
+    )
+
+    def __init__(self, routes_dir):
+        super().__init__(self.get_routes(routes_dir).items())
+
+    def generate_route_name(self, module_name):
+        module_name = module_name.replace('.', '/')
+        if module_name.endswith('index'):
+            module_name = module_name[:-len('index')]
+
+        if len(module_name) > 1 and module_name.endswith('/'):
+            module_name = module_name[:-1]
+
+        if not module_name.startswith('/'):
+            module_name = '/{}'.format(module_name)
+
+        return module_name
+
+    def extract_methods(self, functions):
+        for name, function in functions:
+            name = name.upper()
+            if name in self.valid_http_methods:
+                yield name, function
+
+    def get_routes(self, routes_dir):
+        routes = {}
+        for module_name, functions in walk(routes_dir):
+            route_name = self.generate_route_name(module_name)
+            msg = 'Duplicate route for `{}` is not allowed!'
+            assert route_name not in routes, msg.format(route_name)
+            routes[route_name] = dict(self.extract_methods(functions))
+        return routes
